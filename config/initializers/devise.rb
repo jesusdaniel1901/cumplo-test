dc00af64039d4601a9e8f3162fd960c3f4132003
Jesus Garcia <jesusdaniel1901@gmail.com>
2017-08-19 21:24:05 -0300

    compiled asset

---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+Devise.setup do |config|
+  config.secret_key = '59e89cfa382f167dc371a0f38ab46b945544a2cf8c0e7b5ac507b2e60c61646de025f121cbbd47fb508b1e1f7442c2fa6ada85ef94df2f83443e005c1b596ed4'
+end
